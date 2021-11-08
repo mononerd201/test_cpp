a608b66d45d9ec6ab265a0c48a3dfcb6f4ee0a72
mononerd201 <09thompjam@gmail.com>
2021-11-08 19:23:39 +0000

    got glfw linked in CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
 #include "Adder/adder.h"
 
 int main()
 {
 	std::cout<<"Hello, World!\n";
 	std::cout<<"adding 2 and 3: "<<add(2,3)<<"\n";
+	
+	GLFWwindow* window;
+
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	window = glfwCreateWindow(640,480,"Hello World",NULL,NULL);
+
+	if(!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	
+	while (!glfwWindowShouldClose(window))
+	{
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
+
+
