c7c04d5927d04f994f48e24baba0b4b2db92a4b0
mononerd201 <09thompjam@gmail.com>
2021-11-08 19:39:07 +0000

    appended CMakeLists.txt to check, install and update glfw submodule

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
-#include "Adder/adder.h"
+
 
 int main()
 {
 	std::cout<<"Hello, World!\n";
-	std::cout<<"adding 2 and 3: "<<add(2,3)<<"\n";
+
 	
 	GLFWwindow* window;
 
